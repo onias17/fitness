cb1b15a0c75f5ec9a1f82187b2d5b5da601213d0
onias17 <oniasnephiisrael@gmail.com>
2021-05-05 00:13:30 -0700

    created super user

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -8,3 +10,13 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+#AUTHORIZATION
+# def signup(request):
+#     error_message = ''
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('new_profile')
