bb2a7c5cefef42593a19330e868cfd281efc4986
onias17 <oniasnephiisrael@gmail.com>
2021-05-05 09:32:56 -0700

    set up profiles urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
 
+    #PROFILES
+    path('profiles/index/', views.profiles_index, name='profiles_index'),
+    path('profiles/new/', views.new_profile, name='new_profile'),
+    path('profiles/new/', views.new_profile, name='new_profile'),
+
     path('accounts/signup', views.signup, name='signup'),
 ]
